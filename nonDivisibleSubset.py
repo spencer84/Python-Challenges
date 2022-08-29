527bedbde513fffb39b8b355315261273569a265
Sam Spencer <sam95@vt.edu>
2022-08-29 22:24:14 +0100

    Working through a way to find the largest non-divisible subset; Not sure this solution will work though..

---
+++ nonDivisibleSubset.py
@@ -0,0 +1,44 @@
+def nonDivisibleSubset(k, s):
+    div_dict = {}
+    max_nondivisors = []
+    for i in s:
+        # Number of pairs that are non-divisible by K
+        nondivisors = 0
+        div_dict[i] = []
+        # Remove the current value and iterate through others
+        s.remove(i)
+        for j in s:
+            if (i + j) % k != 0:
+                nondivisors += 1
+                div_dict[i].append(j)
+        max_nondivisors.append(nondivisors)
+        s.insert(0, i)
+    max_vals = sorted(list(set(max_nondivisors)), reverse=True)
+    endnotfound = True
+    while endnotfound:
+        pair_counts = {}
+        for val in div_dict:
+            if len(div_dict[val]) >= max_vals[0]:
+                for i in div_dict[val]:
+                    if i in pair_counts:
+                        pair_counts[i] += 1
+                    else:
+                        pair_counts[i] = 1
+        print(len(pair_counts))
+        print(pair_counts)
+        if len(pair_counts) >= max_vals[0]:
+            endnotfound = False
+            max_vals.remove(max_vals[0])
+        else:
+            pass
+
+    print(len(pair_counts))
+    return len(pair_counts)
+
+
+s0 = [1, 7, 2, 4]
+k0 = 3
+s1 = [278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436]
+k1 = 7
+if __name__ == "__main__":
+    nonDivisibleSubset(k1, s1)
